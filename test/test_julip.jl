65d138940b55d6aefe531ec878e400c8752860ea
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-01-11 21:53:46 +0000

    fixed some tests

--- test/test_julip.jl
+++ test/test_julip.jl
@@ -5,7 +5,28 @@ using Base.Test
 X_Ti = vecs([0.0 5.19374 2.59687 3.8953 1.29843 6.49217 7.7906 12.9843 10.3875 11.6859 9.08904 14.2828; 0.0 0.918131 1.83626 -1.11022e-16 0.918131 1.83626 -2.22045e-16 0.918131 1.83626 0.0 0.918131 1.83626; 0.0 0.0 0.0 2.24895 2.24895 2.24895 0.0 0.0 0.0 2.24895 2.24895 2.24895])
 C_Ti = (@SMatrix [15.5812 2.47895 0.0; 0.0 2.75439 0.0; 0.0 0.0 4.49791])
 
-pynlist(at, cutoff) = ASE.matscipy_nlist(at, cutoff)
+# -------------- MatSciPy NeighbourList Patch -------------
+using PyCall
+import NeighbourLists
+matscipy_neighbours = pyimport("matscipy.neighbours")
+function asenlist(at::Atoms, rcut)
+   pyat = ASEAtoms(at).po
+   return matscipy_neighbours[:neighbour_list]("ijdD", pyat, rcut)
+end
+
+function matscipy_nlist(at::Atoms{T}, rcut::T; recompute=false, kwargs...) where T <: AbstractFloat
+   i, j, r, R = asenlist(at, rcut)
+   i = copy(i)+1
+   j = copy(j)+1
+   r = copy(r)
+   R = vecs(copy(R'))
+   first = NeighbourLists.get_first(i, length(at))
+   NeighbourLists.sort_neigs!(j, r, R, first)
+   return NeighbourLists.PairList(positions(at), rcut, i, j, r, R, first)
+end
+# --------------------------------------------------------
+
+pynlist(at, cutoff) = matscipy_nlist(at, cutoff)
 jnlist(at, cutoff) = PairList(positions(at), cutoff, cell(at), pbc(at))
 
 function test_nlist_julip(at, cutoff)
@@ -77,8 +98,8 @@ rcut = 2.3*rnn(:Si)
 push!(test_configs, ("Si left-oriented", at, rcut))
 push!(test_configs, ("Si left-oriented, large", atlge, rcut))
 
-# [2] vacancy in bulk Si
-
+# # [2] vacancy in bulk Si
+#
 # using PyCall
 # at = bulk(:Si, cubic=true)
 # @pyimport ase.lattice.cubic as cubic
@@ -86,7 +107,7 @@ push!(test_configs, ("Si left-oriented, large", atlge, rcut))
 # at2py[:get_positions]()
 # @test at2py[:get_cell]() ≈ cell(at1)
 # @test mat(positions(at1))' ≈ at2py[:get_positions]()[:,[3,2,1]]
-
+#
 # at = bulk(:Si, cubic=true)
 # at1 = at * 3
 # X = mat(positions(at1))
