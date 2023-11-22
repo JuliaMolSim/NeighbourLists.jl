57519c5abd34ed89247cb0f7a7217b3d00d96582
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2023-11-22 12:07:04 -0800

    AtomsBase PairList creation

---
+++ src/atoms_base.jl
@@ -0,0 +1,19 @@
+using AtomsBase
+using StaticArrays: SVector
+using Unitful
+
+
+function PairList(ab::AtomsBase.AbstractSystem, cutoff::Unitful.Length)
+    length_unit = unit(cutoff)
+    cell = ustrip.(length_unit, hcat( bounding_box(ab)... )' )
+    pbc = map( boundary_conditions(ab) ) do x
+        x == Periodic()
+    end
+    r = map( 1:length(ab)) do i
+        # Need to have SVector here for PairList to work
+        # if position does not give SVector
+        SVector( ustrip.(length_unit, position(ab,i))...)
+    end
+    nlist = PairList(r, ustrip(length_unit, cutoff), cell, pbc; int_type=Int)
+    return nlist
+end
